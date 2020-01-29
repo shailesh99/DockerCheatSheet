ef05fcb8b9ca7036a42e51cab082d0ecbbed811c
shailesh99 <shailesh99@gmail.com>
2020-01-28 18:05:33 -0600

    Run an Ubuntu container and keep it running by running sleep

--- DockerCheatSheet.sh
+++ DockerCheatSheet.sh
@@ -9,3 +9,6 @@ sudo docker-machine scp -r ./example/ myvm1:/home/app
 docker image tag ubuntu:latest shailesh99/myubuntu:latest
 #then push the image
 docker push shailesh99/myubuntu:tagname
+
+#run Ubuntu container and keep it running
+docker run -d --rm --name ubuntu shailesh99/myubuntu sleep 1000
