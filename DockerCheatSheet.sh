55b3e6ceedabe258385a2c18709e803a3c9fcb55
shailesh99 <shailesh99@gmail.com>
2020-01-31 12:36:53 -0600

    docker build

--- DockerCheatSheet.sh
+++ DockerCheatSheet.sh
@@ -2,13 +2,20 @@
 #Docker Machine Copy
 sudo docker-machine scp -r ./example/ myvm1:/home/app
 
+#Build the image
+#docker build -t [repository]/(image):[tag]
+docker build -t shailesh99/myubuntu:1.1
 
 #Push
 #First tag a local image with the new repo and tag
 #docker image tag local_image:tag new_repo/image:tag
-docker image tag ubuntu:latest shailesh99/myubuntu:latest
+docker image tag ubuntu:latest shailesh99/myubuntu:1.1
 #then push the image
-docker push shailesh99/myubuntu:tagname
+docker push shailesh99/myubuntu:1.1
 
 #run Ubuntu container and keep it running
 docker run -d --rm --name ubuntu shailesh99/myubuntu sleep 1000
+docker run -d --rm --name ubuntuc ubuntu top -c -b
+
+
+
